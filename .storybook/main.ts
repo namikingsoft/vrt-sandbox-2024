c12dd6b9a8d992f1c427736cd24feb20e98d38d8
Tsubasa Namiki <namikingsoft@gmail.com>
2024-09-13 16:20:12 +0900

    Avoid needless plugins

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,10 +6,8 @@ const config: StorybookConfig = {
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
-    "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
     "@storybook/addon-interactions",
   ],
   framework: {
