081a5e081df4fdaf22df714ed8fe59a513fec830
Tsubasa Namiki <namikingsoft@gmail.com>
2024-09-22 03:04:45 +0900

    Reduce stories args

--- components/Button.stories.tsx
+++ components/Button.stories.tsx
@@ -1,21 +1,18 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
-
 import { Button } from "./Button";
 
 const meta = {
-  title: "components/Button",
   component: Button,
-  args: { onClick: fn() },
+  args: {
+    color: "primary",
+    onClick: fn(),
+    children: <>Button</>,
+  },
 } satisfies Meta<typeof Button>;
 
 type Story = StoryObj<typeof meta>;
 
 export default meta;
 
-export const Default: Story = {
-  args: {
-    color: "default",
-    children: "Default",
-  },
-};
+export const Basic: Story = {};
