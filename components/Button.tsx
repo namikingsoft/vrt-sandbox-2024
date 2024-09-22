2c7848652edaccee90ce2a06600fb11aa7b99146
Tsubasa Namiki <namikingsoft@gmail.com>
2024-09-23 01:23:11 +0900

    Update button style

--- components/Button.tsx
+++ components/Button.tsx
@@ -21,7 +21,7 @@ const colorClasses = (color: Props["color"]) => {
 export const Button = ({ color, onClick, children }: Props) => {
   return (
     <button
-      className={clsx(colorClasses(color), "rounded", "p-2")}
+      className={clsx(colorClasses(color), "rounded", "p-3")}
       onClick={onClick}
     >
       {children}
