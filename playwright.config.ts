7d2c434754bf1222ab71656db9dfb0dbed6dcdad
Tsubasa Namiki <namikingsoft@gmail.com>
2025-07-02 06:48:01 +0900

    Follow latest workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,4 +2,13 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testMatch: /.*\.e2e\.ts/,
+  workers: process.env.CI ? 1 : undefined,
+  webServer: {
+    command: "yarn start-storybook-vrt",
+    url: "http://127.0.0.1:8080",
+  },
+  expect: {
+    timeout: 10 * 1000,
+  },
+  retries: 3,
 });
